58fa688511b472b8eb697a8c95983b73f8dbba06
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2024-04-07 20:25:33 +0500

    Story Component Updated

--- components/containers/Story.jsx
+++ components/containers/Story.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FullContainer from "../common/FullContainer";
 import Container from "../common/Container";
+import axios from "axios";
+import MarkdownIt from "markdown-it";
 
-export default function Story({ data }) {
-  const storyData = data?.find((item) => item.title === "story");
+export default function Story() {
+  const [storyData, setStoryData] = useState({});
+  const fetchStory = async () => {
+    try {
+      const response = await axios.get(
+        `${
+          process.env.NEXT_PUBLIC_SITE_MANAGER
+        }/public/industry_template_data/${
+          process.env.NEXT_PUBLIC_INDUSTRY_ID
+        }/${process.env.NEXT_PUBLIC_TEMPLATE_ID}/data/${"Story"}`
+      );
+      setStoryData(response.data.data[0].value);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStory();
+  }, []);
+
+  const markdownIt = new MarkdownIt();
+  const convertMarkdown = (markdownText) => markdownIt?.render(markdownText);
+  console.log("Story Data", typeof storyData?.text);
+
   return (
     <FullContainer>
-      <Container className="text-xl py-16">{storyData.text}</Container>
+      <Container className="text-xl py-16">
+        <div
+          className="w-full text-left text-lg mt-4 markdown-content"
+          dangerouslySetInnerHTML={{
+            __html: convertMarkdown(storyData?.text?.toString()),
+          }}
+        />
+      </Container>
     </FullContainer>
   );
 }
